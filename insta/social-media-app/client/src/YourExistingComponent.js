0ff92fdca9cc64eb4609425aea32b909fffa6094
SatyaDev <66716008+programmingxpert@users.noreply.github.com>
2024-08-02 11:19:17 +0530

    Delete insta directory

--- insta/social-media-app/client/src/YourExistingComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LandingPage from './LandingPage'; // Ensure the import is correct
-
-const YourExistingComponent = () => {
-  return <LandingPage />;
-};
-
-export default YourExistingComponent;
